65e17f3170b6de6059b5f2157a2faa838d1dbccd
AtulRanjan <atulranjan164@gmail.com>
2024-04-01 21:27:35 +0530

    add js project

---
+++ conditional.js
@@ -0,0 +1,51 @@
+// let x = 51 ;
+// if(x%2==0){
+//     console.log("even")
+
+// }
+// else{
+//     console.log("odd")
+// }
+
+// for(let i =1000 ;i>=1 ; i--){
+//     document.write(i +"<br>");
+// }
+
+
+// TERNARY OPERATOR
+// let x = "atul";
+// x = (100%2 ===0)?"Ranjan" :"sonu" ; 
+// console.log(x);
+
+
+
+// let x = "atul";
+// if(101%2 ===0){
+//     x = "Ranjan";
+//     console.log(x);
+// }
+// else{
+//     x = "sonu";
+//     console.log(x);
+// }
+
+
+// function
+// function sum (a ,b ){
+//     let s = a+b;
+//     console.log(s);
+// }
+// sum (4,4);
+// sum (4,290);
+// sum(4)
+
+// let n = "atul"
+// n = "Ranjan"
+// n = "ruddu"
+// console.log(n)
+
+let nums  = [3,4,5,6,7];
+console.log(nums[2]+nums[3]+nums[nums.length-1])
+nums.push() // add the vlaue end 
+nums.unshift(8,9)
+console.log(nums)
