ee25afee5b7499d615e048cdcfc813701a7944c1
AtulRanjan <atulranjan164@gmail.com>
2024-04-02 20:46:34 +0530

    add

---
+++ array/pattern/String/class1.js
@@ -0,0 +1,20 @@
+let a = "Rohit"
+let b = "100"
+
+let str  = "javascript is Awesome";
+console.log(str.indexOf("a"))
+console.log(str.indexOf("J")) // ans is -1 means ans is not fond 
+console.log(str.lastIndexOf("is"))
+
+let str2 = "abcdabc"
+console.log(str2.lastIndexOf())
+
+
+
+//give ans true and false 
+
+let str3 = "Atul  is student of geekster"
+console.log(str3.includes("atul")) // becouse java is a casesensetive language
+console.log(str3.includes("Atul"))
+
+Console.log(str.slice(0, 6));
