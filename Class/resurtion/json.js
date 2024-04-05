d09ca542e9c65af82b47beef1f958e480fe82bad
AtulRanjan <atulranjan164@gmail.com>
2024-04-05 20:12:24 +0530

    add some message

---
+++ Class/resurtion/json.js
@@ -0,0 +1,43 @@
+// const name = ["atul", "ayush" ,"sone " ,"Abhinandan"];
+
+// for of loop use 
+// for(let i of name ){
+//     console.log(i);
+// }
+
+// ForEach 
+// const names= ["atul", "ayush" ,"sone " ,"Abhinandan"];
+// names.forEach(printname);
+// function printname (names){
+//     // console.log("hello")  // PRint the vkaye of four time 
+//     console.log(names)
+// }
+
+
+
+// const num  = [12,32,33,434,43,4323,];
+// let odd = [];
+// for (let i = 0  ; i< num.length ; i++){
+//     if(num[i]%2===1){
+//         odd.push(num[i])
+//     }
+// }
+// console.log(odd);
+
+
+// find the largest number from the array using foreach & Array function 
+const names =["Ayushi" , "abhinandan" ,"atul" , "Ravinder"]
+let largestname = "";
+names.forEach((name) => {
+if(name.length>largestname.length) largestname = name;
+});
+
+console.log(largestname);
+
+
+
+const a1 = [2,3,4,5];
+const a2 = [1,2,3,4,5]``
+
+
+
---
+++ Class/resurtion/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+onst names= ["atul", "ayush" ,"sone " ,"Abhinandan"];
+// names.forEach(printname);
+// function printname (names){
+//     // console.log("hello")  // PRint the vkaye of four time 
+//     console.log(names)
+// }
