211f7ba0f3c0efb1dddd83cb7c232760377a96d1
AtulRanjan <atulranjan164@gmail.com>
2024-04-13 10:33:42 +0530

    add

---
+++ Project/Student App/student.js
@@ -0,0 +1,123 @@
+const studentdata = 
+    [
+        {
+            "name": "Aarav",
+            "age": 21,
+            "skills": ["Java", "HTML", "CSS"]
+            },
+            
+            {
+            "name": "Neha",
+            "age": 20,
+            "skills": ["C++", "JavaScript", "React"]
+            },
+            
+            {
+            "name": "Rohan",
+            "age": 22,
+            "skills": ["Python", "SQL", "Django"]
+            },
+            
+            {
+            "name": "Priya",
+            "age": 23,
+            "skills": ["C", "Ruby", "Rails"]
+            },
+            
+            {
+            "name": "Arjun",
+            "age": 21,
+            "skills": ["JavaScript", "Node.js", "MongoDB"]
+            },
+            
+            {
+            "name": "Sneha",
+            "age": 20,
+            "skills": ["Python", "Flask", "SQLite"]
+            },
+            
+            {
+            "name": "Vikram",
+            "age": 22,
+            "skills": ["Java", "Spring", "Hibernate"]
+            },
+            
+            {
+            "name": "Pooja",
+            "age": 23,
+            "skills": ["C#", "ASP.NET", "MSSQL"]
+            },
+            
+            {
+            "name": "Amit",
+            "age": 21,
+            "skills": ["JavaScript", "Vue.js", "Firebase"]
+            },
+            
+            {
+            "name": "Anjali",
+            "age": 22,
+            "skills": ["Python", "Django", "PostgreSQL"]
+            },
+            
+            {
+            "name": "Rahul",
+            "age": 20,
+            "skills": ["C", "Angular", "MongoDB"]
+            },
+            
+            {
+            "name": "Smita",
+            "age": 23,
+            "skills": ["Java", "Spring Boot", "MySQL"]
+            },
+            
+            {
+            "name": "Aditya",
+            "age": 21,
+            "skills": ["Python", "Flask", "SQLite"]
+            },
+            
+            {
+            "name": "Arun",
+            "age": 20,
+            "skills": ["JavaScript", "React", "Redux"]
+            },
+            
+            {
+            "name": "Divya",
+            "age": 22,
+            "skills": ["Python", "Django", "PostgreSQL"]
+            },
+            
+            {
+            "name": "Kiran",
+            "age": 23,
+            "skills": ["C", "Angular", "MongoDB"]
+            }
+            ,
+            {
+            "name": "Manoj",
+            "age": 21,
+            "skills": ["Java", "Spring Boot", "MySQL"]
+            },
+            
+            {
+            "name": "Swati",
+            "age": 22,
+            "skills": ["Python", "Flask", "SQLite"]
+            },
+            
+            {
+            "name": "Raj",
+            "age": 20,
+            "skills": ["JavaScript", "React", "Redux"]
+            },
+            
+            {
+            "name": "Aarti",
+            "age": 23,
+            "skills": ["Python", "Django", "PostgreSQL"]
+            }
+                    
+    ]
